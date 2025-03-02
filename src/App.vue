2427de38915ed250656d630c223988949f76efe9
Kizito Mrema <kizomanizo@gmail.com>
2025-03-02 22:42:39 +0300

    Closes #1; Bootstraped the app

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import LoginView from './views/LoginView.vue'
+import DashboardView from './views/DashboardView.vue'
+import { useAuthStore } from './stores/auth'
+import { computed } from 'vue'
+
+const auth = useAuthStore()
+const isAuthenticated = computed(() => auth.isAuthenticated)
+</script>
+
+<template>
+  <header>
+    <div class="wrapper"> <!--Will add if no login -->
+      <LoginView v-if="!isAuthenticated" msg="Please login first" />
+      <DashboardView v-else />
+    </div>
+  </header>
+
+  <footer>
+    <p>&copy; 2025</p>
+  </footer>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+</style>
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h2 class="text-4xl font-extrabold">Dashboard</h2>
+  </main>
+</template>
