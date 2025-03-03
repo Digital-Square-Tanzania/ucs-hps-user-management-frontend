dd76bba7f4b77e1ff4607508397d0074804d3e26
Kizito Mrema <kizomanizo@gmail.com>
2025-03-04 01:15:11 +0300

    Closes #2; Completed login page and basic ui tools for reuse

---
+++ src/components/ui/TransparentCover.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="authStore.loading" class="fixed inset-0 bg-white opacity-80 flex items-center justify-center z-49">
+    <div class="text-black flex flex-col items-center justify-center">
+      <SettingsCog class="animate-spin [animation-duration:2.4s]" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import SettingsCog from '@/components/vectors/SettingsCog.vue'
+
+const authStore = useAuthStore()
+</script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,29 @@
 <template>
-  <main>
+  <header class="flex flex-row items-center justify-between">
     <h2 class="text-4xl font-extrabold">Dashboard</h2>
+    <p class="text-sm font-medium cursor-pointer" @click="logout">Logout</p>
+  </header>
+  <main>
+
   </main>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+import { useAlertStore } from '@/stores/alert'
+
+
+const authStore = useAuthStore()
+const alertStore = useAlertStore()
+
+const logout = () => {
+  authStore.logout();
+  alertStore.showAlert('warning', 'Goodbye', 'You\'ve successfully logged out!')
+};
+
+onMounted(() => {
+  alertStore.showAlert('success', 'Welcome', 'You\'ve successfully logged in!')
+})
+
+</script>
