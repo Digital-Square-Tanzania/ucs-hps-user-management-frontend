4b4fe0648bc30bcafb009415760e6381cb05b478
Kizito Mrema <kizomanizo@gmail.com>
2025-03-18 01:35:08 +0300

    FEAT: Added a loader screen

---
+++ src/components/layout/TransparentCover.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="fixed inset-0 bg-white opacity-50 backdrop-blur-2xl flex items-center justify-center z-55">
+    <div class="spinner">
+      <ProgressSpinner />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ProgressSpinner } from 'primevue';
+
+</script>
